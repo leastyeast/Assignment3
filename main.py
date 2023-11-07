75870d73c4da6c65a29ba0fd9cabc880ecc3e893
leastyeast <Katherine.Kisiloski@edu.sait.ca>
2023-11-07 15:59:32 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# introductory formatting
+print("----------------------------------------------------")
+print("*** Welcome to Park and Go Parking Application! ***")
+print("----------------------------------------------------")
+
+# menu formatting
+print("Select from the following options:\n\
+1- Register a vehicle\n\
+2- Verify vehicle registration\n\
+3- Display registered vehicles and save them to a file\n\
+4- Display dialy charges and save them to a file\n\
+5- Remove a vehicle\n\
+6- Clear vehicles\n\
+0- Exit")
+
+choice = input(">>> ")
