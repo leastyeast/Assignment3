4de336719b96d16dc9eecb29c82e3477e33e08b0
leastyeast <Katherine.Kisiloski@edu.sait.ca>
2023-11-16 19:16:47 -0700

    step 2

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-# introductory formatting
-print("----------------------------------------------------")
-print("*** Welcome to Park and Go Parking Application! ***")
-print("----------------------------------------------------")
-
 def print_menu():
     # menu formatting
-    menu = "Select from the following options:\n\
+    menu = "----------------------------------------------------\n\
+*** Welcome to Park and Go Parking Application! ***\n\
+----------------------------------------------------\n\
+Select from the following options:\n\
 1- Register a vehicle\n\
-2 - Verify vehicle registration\n\
+2- Verify vehicle registration\n\
 3- Display registered vehicles and save them to a file\n\
-4- Display dialy charges and save them to a file\n\
+4- Display daily charges and save them to a file\n\
 5- Remove a vehicle\n\
 6- Clear vehicles\n\
 0- Exit"
@@ -18,6 +16,8 @@ def print_menu():
 print(print_menu())
 
 choice = int(input(">>> "))
+
+#
 
 if choice == 1:
     pass
@@ -31,6 +31,15 @@ elif choice == 5:
     pass
 elif choice == 6:
     pass
+elif choice == 0:
+    pass
+else:
+    print("Invalid input.")
+    input1 = input("Press enter to continue....")
+    if input1 == "":
+        print(print_menu())
+    else:
+        print("Please press enter.")
 
 def check_password():
     password_input = input("Please enter your password: ").lower()
@@ -39,5 +48,3 @@ def check_password():
     elif password_input is not "password":
         output = "Password is incorrect!"
         return output
-
-print(check_password())
