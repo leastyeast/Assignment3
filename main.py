eb0191992ad318510a141da550a8c1675c38c478
leastyeast <Katherine.Kisiloski@edu.sait.ca>
2023-11-16 20:02:42 -0700

    completed menu loop

--- main.py
+++ main.py
@@ -17,29 +17,37 @@ print(print_menu())
 
 choice = int(input(">>> "))
 
-#
-
-if choice == 1:
-    pass
-elif choice == 2:
-    pass
-elif choice == 3:
-    pass
-elif choice == 4:
-    pass
-elif choice == 5:
-    pass
-elif choice == 6:
-    pass
-elif choice == 0:
-    pass
-else:
+while choice != 0 and choice != 1 and choice != 2 and choice != 3 and choice != 4 and choice != 5 and choice != 6:
+    #invalid choices
     print("Invalid input.")
     input1 = input("Press enter to continue....")
     if input1 == "":
         print(print_menu())
+        choice = int(input(">>> "))
     else:
-        print("Please press enter.")
+        print("Please input enter.")
+    #valid choices
+    if choice == 1:
+        print("1")
+        continue
+    elif choice == 2:
+        print("2")
+        continue
+    elif choice == 3:
+        print("3")
+        continue
+    elif choice == 4:
+        print("4")
+        continue
+    elif choice == 5:
+        print("5")
+        continue
+    elif choice == 6:
+        print("6")
+        continue
+    elif choice == 0:
+        print("0")
+        continue
 
 def check_password():
     password_input = input("Please enter your password: ").lower()
