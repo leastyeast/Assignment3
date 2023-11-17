5a83592a12c6f74deff735011565af0b7af30039
leastyeast <Katherine.Kisiloski@edu.sait.ca>
2023-11-16 22:15:39 -0700

    step 3

--- main.py
+++ main.py
@@ -12,13 +12,41 @@ Select from the following options:\n\
 6- Clear vehicles\n\
 0- Exit"
     return menu
-
+#original print menu
 print(print_menu())
 
+#first input check
 choice = int(input(">>> "))
 
+#password check function
+def check_password():
+    password_input = input("Please enter your password: ").lower()
+    if password_input == "password":
+        return None
+    elif password_input != "password":
+        output = "Password is incorrect!"
+        return output
+
+#registering vehicles
+def register_vehicle():
+    i = 0
+    if i >= 50:
+        print("The parking lot has no spaces. PLease try again later")
+    elif i < 50:
+        print("The parking lot has spaces")
+        plate_number = []
+        credit_card_number = []
+        
+        plate_number = input("Please input your plate number: ")
+        credit_card_number = input("Enter your Credit Card Number (4.00$ charge): ")
+        
+        print(f"Thank you, your plate {plate_number} has been added to the lot.")
+
+#displaying vehicles
+def display_vehicles():
+    pass
+
 while choice != 0 and choice != 1 and choice != 2 and choice != 3 and choice != 4 and choice != 5 and choice != 6:
-    #invalid choices
     print("Invalid input.")
     input1 = input("Press enter to continue....")
     if input1 == "":
@@ -27,32 +55,44 @@ while choice != 0 and choice != 1 and choice != 2 and choice != 3 and choice != 
     else:
         print("Please input enter.")
     #valid choices
+while choice == 0 or choice == 1 or choice == 2 or choice == 3 or choice == 4 or choice == 5 or choice == 6:
     if choice == 1:
-        print("1")
+        register_vehicle()
+
+        input1 = input("Press enter to continue....")
+        if input1 == "":
+            print(print_menu())
+            choice = int(input(">>> "))
+        else:
+            print("Please input enter.")
         continue
     elif choice == 2:
-        print("2")
+        #verify_vehicle()
         continue
     elif choice == 3:
-        print("3")
+        check_password()
+        display_vehicles()
+
+        input1 = input("Press enter to continue....")
+        if input1 == "":
+            print(print_menu())
+            choice = int(input(">>> "))
+        else:
+            print("Please input enter.")
         continue
     elif choice == 4:
-        print("4")
+        check_password()
+        #diplay_charges()
         continue
     elif choice == 5:
-        print("5")
+        #remove_vehicle()
         continue
     elif choice == 6:
-        print("6")
+        #clear_vehicles()
         continue
     elif choice == 0:
-        print("0")
-        continue
-
-def check_password():
-    password_input = input("Please enter your password: ").lower()
-    if password_input == "password":
-        return None
-    elif password_input is not "password":
-        output = "Password is incorrect!"
-        return output
+        print("Thank you, goodbye!")
+        break
+    else:
+        print("Invalid input.")
+        input = input("Press enter to continue....")
